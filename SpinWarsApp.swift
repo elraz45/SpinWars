60565bf0d5f64e7a23ce4a4244c12d6d5645161f
Elraz <elrazh322@gmail.com>
2024-10-09 01:01:00 +0300

    another commit

---
+++ SpinWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinWarsApp.swift
+//  SpinWars
+//
+//  Created by Elraz Hamtsani on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpinWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
